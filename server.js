c6892873d1d8498fc013b35a1e7070b062bbdf35
cberigan <cberigan@amherst.com>
2018-09-12 13:50:45 -0500

    s

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 
-var  ledToggle, pressCount;
+var  ledRedToggle, ledGreenToggle, pressCount;
 //provision the gpio pins 22 for the led output and 17 for the button input
-var led= require("pi-pins").connect(22),
+var ledRed = require("pi-pins").connect(22),
+    ledGreen = require("pi-pins").connect(27),
     button = require("pi-pins").connect(17);
 
 var app = express();
@@ -11,12 +12,17 @@ var app = express();
 
 //set the pin mode,  setting pin 22 as an output and 17 as an input
 button.mode('in');
-led.mode('out');
+ledRed.mode('out');
+ledGreen.mode('out');
 
 //set the initial value of the LED to be off.
-ledToggle = false;
+ledRedToggle = false;
+ledGreenToggle = false;
+
 pressCount= 0;
-led.value(true);
+
+ledRed.value(false);
+ledGreen.value(false);
 
 
 // reply to request with "Hello World!"
@@ -25,11 +31,18 @@ app.get('/', function (req, res) {
   res.send("button pressed: "+ (pressCount) +" time(s)");
 });
 
+
+//toggle green light
+app.post('/toggle', function (req, res) {
+    ledGreenToggle = !ledGreenToggle;
+    ledGreen.value(ledGreenToggle);
+  })
+
 //look for a button press event and switch on the LED for 2 seconds when this happens.
 button.on('rise', function () {
-    ledToggle = !ledToggle;
+    ledRedToggle = !ledRedToggle;
     pressCount++;
-    led.value(ledToggle);
+    ledRed.value(ledRedToggle);
 });
 
 //start a server on port 80 and log its start to our console
